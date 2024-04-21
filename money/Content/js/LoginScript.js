607c5c494cbd6f72488a8cab5d462eb75813b8b4
Nuwan Dilina Hettiarachchi <nuwan.dilina@hayleysadvantis.com>
2024-04-21 09:46:39 +0530

    project setup and tempate add

---
+++ money/Content/js/LoginScript.js
@@ -0,0 +1,117 @@
+﻿function functionLogin() {
+    debugger
+    var txtEmailBox = document.getElementById("txtEmail");
+    var txtPasswordBox = document.getElementById("txtPassword");
+    var txtEmail = txtEmailBox.value; debugger
+    var txtPassword = txtPasswordBox.value;
+    if (txtEmail != '' && txtPassword != '') {
+        window.location.href = "/Home/Index";
+    }
+    else {
+        functionLoginValidation()
+    }
+}
+function functionLoginValidation() {
+    var txtEmailBox = document.getElementById("txtEmail");
+    var txtPasswordBox = document.getElementById("txtPassword");
+    var txtEmail = txtEmailBox.value; 
+    var txtPassword = txtPasswordBox.value;
+    if (txtEmail == '') {
+        txtEmailBox.style.borderColor = "red";
+    }
+    else {
+        txtEmailBox.style.borderColor = "green";
+    }
+    if (txtPassword == '') {
+        txtPasswordBox.style.borderColor = "red";
+    }
+    else {
+        txtPasswordBox.style.borderColor = "green";
+    }
+    if (txtEmail != '' && txtPassword != '') {
+
+    }
+};
+
+var txtOTP = document.getElementById("txtOTP");
+var txtPassword = document.getElementById("txtPassword");
+var txtPasswordConfirm = document.getElementById("txtPasswordConfirm");
+var btnResetPassword = document.getElementById("btnResetPassword");
+var btnSendReset = document.getElementById("btnSendReset");
+var txtReturnOTPVal = "12345678";
+function functionForgotPasswordLoad() {
+    txtOTP.disabled = true;
+    txtPassword.disabled = true;
+    txtPasswordConfirm.disabled = true;
+    btnResetPassword.disabled = true;
+    btnSendReset.disabled = true;
+}
+function functionEmailEnter() {
+    var txtEmailBox = document.getElementById("txtEmail");
+    var txtEmail = txtEmailBox.value;
+    if (txtEmail == '') {
+        btnSendReset.disabled = true;
+        txtEmailBox.style.borderColor = "red";
+    }
+    else {
+        btnSendReset.disabled = false;
+        txtEmailBox.style.borderColor = "green";
+    }
+}
+
+function functionSendResetLink() {
+    txtOTP.disabled = false;
+    txtReturnOTPVal="11111111"
+}
+
+function functionOTPEnter() {
+    var txtOTPval = txtOTP.value;
+    if (txtOTPval.length == 8) {
+        if (txtReturnOTPVal == txtOTPval) {
+            txtPassword.disabled = false;
+            txtPasswordConfirm.disabled = false;
+        }
+        else {
+            txtPassword.disabled = true;
+            txtPasswordConfirm.disabled = true;
+        }
+    }
+}
+
+var txtPasswordval = "";
+var txtConfirmPasswordval = "";
+
+function functionPasswordEnter() {
+    txtPasswordval = txtPassword.value;
+    txtConfirmPasswordval = txtPasswordConfirm.value;
+    if (txtPasswordval == txtConfirmPasswordval) {
+        btnResetPassword.disabled = false;
+    }
+    else {
+        btnResetPassword.disabled = true;
+    }
+}
+function functionConfirmPasswordEnter() {
+    txtPasswordval = txtPassword.value;
+    txtConfirmPasswordval = txtPasswordConfirm.value;
+    if (txtPasswordval == txtConfirmPasswordval) {
+        btnResetPassword.disabled = false;
+    }
+    else {
+        btnResetPassword.disabled = true;
+    }
+}
+
+function functionResetPassword() {
+    var txtOTPval = txtOTP.value;
+    txtPasswordval = txtPassword.value;
+    txtConfirmPasswordval = txtPasswordConfirm.value;
+    if (txtReturnOTPVal == txtOTPval && txtPasswordval == txtConfirmPasswordval) {
+        window.location.href = "/Account/Index";
+    }
+    else {
+        alert("Invalid OTP, Please try again.");
+        window.location.href = "/Account/FogotPassword";
+    }
+    
+}
---
+++ money/Content/js/mainPage.js
@@ -0,0 +1,48 @@
+﻿function formload() {
+    close_nav()
+}
+function close_nav() {
+    var sideBar = document.getElementById('side-bar-id');
+    sideBar.classList.remove('expanded');
+    sideBar.classList.toggle('collapsed');
+
+    var open_div = document.getElementById("side-bar-open-id");
+    open_div.style.display = "block";
+    var close_div = document.getElementById("side-bar-close-id");
+    close_div.style.display = "none";
+
+    var descriptions = document.querySelectorAll('.side-bar-link-desc');
+    descriptions.forEach(function (description) {
+        description.style.display = 'none';
+    });
+
+    var anchors = document.querySelectorAll('.side-bar a');
+    anchors.forEach(function (anchor) {
+        anchor.style.paddingLeft = '10px';
+    });
+
+}
+function open_nav() {
+    debugger
+    var sideBar = document.getElementById('side-bar-id');
+    sideBar.classList.remove('collapsed');
+    sideBar.classList.toggle('expanded');
+
+    var open_div = document.getElementById("side-bar-open-id");
+    open_div.style.display = "none";
+    var close_div = document.getElementById("side-bar-close-id");
+    close_div.style.display = "block";
+
+    var descriptions = document.querySelectorAll('.side-bar-link-desc');
+    descriptions.forEach(function (description) {
+        description.style.display = 'block';
+    });
+
+    var anchors = document.querySelectorAll('.side-bar a');
+    anchors.forEach(function (anchor) {
+        anchor.style.padding = '8px 8px 8px 38px';
+    });
+}
+function sign_out() {
+    window.location.href = '/Account/Index';
+}
